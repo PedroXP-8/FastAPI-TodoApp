0e9d1b5d6d7f59297b756388c4f927671407f33d
Pedro_Henrique <2008phdias@gmail.com>
2026-07-18 18:33:52 -0300

    alter postrgeSQL key

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/TodoApplicationDataBase'
+###SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/TodoApplicationDataBase'###
+SQLALCHEMY_DATABASE_URL = 'postgresql://ph_production_postgresql_user:3nFlOX9OYAAm69wKbbXDhKYa5H8XX5uR@dpg-d9dv1pjrjlhs73bdo1k0-a/ph_production_postgresql'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
